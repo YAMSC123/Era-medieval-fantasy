ab379884b4a3a8fbb0c4edc43af04a21fa6a7887
YAMSC123 <ygor.corso@escola.pr.gov.br>
2025-10-03 09:24:48 -0300

    a

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const avance = document.querySelectorAll('btn-proximo');
 
 avance.forEach(button =>  {
-    button.addEventListener("click", function(){
-        const atual = document.querySelector(".ativo");
-        const proximoPasso = 'passo-' + this.getAttribute("dataProximo");
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-        atual.classList.remove("ativo");
-        document.getElementById(proximoPasso).classList.add("ativo");
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
